af4d2f7fd8f04acb5003445e6d8fd35c9d630736
SLKyrim <623962644@qq.com>
2017-09-25 13:58:00 +0800

    2017-9-25-1
    标注CMO_programmersGuide.pdf中一些重要的属性；
    添加test代码供测试。

---
+++ ExoGaitMonitor - test/ExoGaitMonitor/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExoGaitMonitor
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Methods methods = new Methods();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnClosed(object sender, EventArgs e)
+        {
+            methods.SerialPortClose();
+        }
+
+        private void Frame_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
+    }
+}
